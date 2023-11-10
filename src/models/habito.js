30149789739509b0ff97faa336e6e0e822b7bb02
Nicolas Pardo Machett's <npardom@unal.edu.co>
2023-11-10 11:22:03 -0500

    Revert "consumo del servicio soap para el do habits"
    This reverts commit 2036a1a4c71fd2af9645d71cc41724a398590f89.

--- src/models/habito.js
+++ src/models/habito.js
@@ -7,8 +7,7 @@ const HabitoSchema = new Schema({
     start_date: { type: Date, required: true },
     frequency: { type: Number, required: true },
     is_done: { type: Boolean, required: true },
-    user_id: { type: Number, required: true },
-    number_of_times: { type: Number, required: true }
+    user_id: { type: Number, required: true }
 
 });
 const Habito = mongoose.model('habito', HabitoSchema);
